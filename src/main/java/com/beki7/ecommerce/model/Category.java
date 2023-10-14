a8dea808b14b66fb1e3541fd8b1f802b20ad9424
bereket <bekhonelign@gmail.com>
2023-10-14 12:17:06 -0700

    category model

--- src/main/java/com/beki7/ecommerce/model/Category.java
+++ src/main/java/com/beki7/ecommerce/model/Category.java
@@ -1,4 +1,13 @@
 package com.beki7.ecommerce.model;
 
+import jakarta.persistence.*;
+
+@Entity(name="CATEGORY")
 public class Category {
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    private String name;
 }
