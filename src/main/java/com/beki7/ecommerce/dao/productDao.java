56b8c9fb98157ceee72ddf0bfae20550ad066816
bereket <bekhonelign@gmail.com>
2023-10-14 13:08:44 -0700

    product dao methods

--- src/main/java/com/beki7/ecommerce/dao/productDao.java
+++ src/main/java/com/beki7/ecommerce/dao/productDao.java
@@ -1,4 +1,37 @@
 package com.beki7.ecommerce.dao;
 
+import java.util.List;
+
+import com.beki7.ecommerce.model.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
 public class productDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
+
+    @Transactional
+    public List<Product> getProducts(){
+        return this.sessionFactory.getCurrentSession().createQuery("from PRODUCT").list();
+    }
+
+    @Transactional
+    public Product addProduct(Product product) {
+        this.sessionFactory.getCurrentSession().save(product);
+        return product;
+    }
+
+    @Transactional
+    public Product getProduct(int id) {
+        return this.sessionFactory.getCurrentSession().get(Product.class, id);
+    }
+
 }
--- src/main/java/com/beki7/ecommerce/dao/userDao.java
+++ src/main/java/com/beki7/ecommerce/dao/userDao.java
@@ -6,9 +6,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class userDao {
 
     @Autowired
