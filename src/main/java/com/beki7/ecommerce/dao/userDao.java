a6761e4eac6a659353e2b535a78c137c59d3ee2d
bereket <bekhonelign@gmail.com>
2023-10-14 13:06:57 -0700

    user service update and userdao

--- src/main/java/com/beki7/ecommerce/dao/userDao.java
+++ src/main/java/com/beki7/ecommerce/dao/userDao.java
@@ -1,4 +1,54 @@
 package com.beki7.ecommerce.dao;
 
+import com.beki7.ecommerce.model.User;
+import jakarta.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class userDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
+    @Transactional
+    public List<User> getAllUser() {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<User> userList = session.createQuery("from CUSTOMER").list();
+        return userList;
+    }
+
+    @Transactional
+    public User saveUser(User user) {
+        this.sessionFactory.getCurrentSession().saveOrUpdate(user);
+        System.out.println("User added" + user.getId());
+        return user;
+    }
+
+    @Transactional
+    public User getUser(String username,String password) {
+        Query query = sessionFactory.getCurrentSession().createQuery("from CUSTOMER where username = :username");
+        query.setParameter("username",username);
+
+        try {
+            User user = (User) query.getSingleResult();
+            System.out.println(user.getPassword());
+            if(password.equals(user.getPassword())) {
+                return user;
+            }else {
+                return new User();
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            User user = new User();
+            return user;
+        }
+
+    }
 }
--- src/main/java/com/beki7/ecommerce/service/userService.java
+++ src/main/java/com/beki7/ecommerce/service/userService.java
@@ -1,56 +1,24 @@
 package com.beki7.ecommerce.service;
 
-import com.beki7.ecommerce.model.User;
-import jakarta.transaction.Transactional;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.hibernate.query.Query;
-
-
 import java.util.List;
 
-@Repository
+import com.beki7.ecommerce.model.User;
+import com.beki7.ecommerce.dao.userDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 public class userService {
     @Autowired
-    private SessionFactory sessionFactory;
+    private userDao userDao;
 
-    public void setSessionFactory(SessionFactory sf) {
-        this.sessionFactory = sf;
-    }
-    @Transactional
-    public List<User> getAllUser() {
-        Session session = this.sessionFactory.getCurrentSession();
-        List<User> userList = session.createQuery("from CUSTOMER").list();
-        return userList;
+    public List<User> getUsers(){
+        return this.userDao.getAllUser();
     }
 
-    @Transactional
-    public User saveUser(User user) {
-        this.sessionFactory.getCurrentSession().saveOrUpdate(user);
-        System.out.println("User added" + user.getId());
-        return user;
+    public User addUser(User user) {
+        return this.userDao.saveUser(user);
     }
 
-    @Transactional
-    public User getUser(String username,String password) {
-        Query query = sessionFactory.getCurrentSession().createQuery("from CUSTOMER where username = :username");
-        query.setParameter("username",username);
-
-        try {
-            User user = (User) query.getSingleResult();
-            System.out.println(user.getPassword());
-            if(password.equals(user.getPassword())) {
-                return user;
-            }else {
-                return new User();
-            }
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-            User user = new User();
-            return user;
-        }
-
+    public User checkLogin(String username,String password) {
+        return this.userDao.getUser(username, password);
     }
 }
