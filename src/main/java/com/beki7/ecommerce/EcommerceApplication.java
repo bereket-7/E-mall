03fe6f6e1ae635a8747381aea52fdacec038f82f
bereket <bekhonelign@gmail.com>
2023-10-14 12:43:23 -0700

    application property file

--- src/main/java/com/beki7/ecommerce/EcommerceApplication.java
+++ src/main/java/com/beki7/ecommerce/EcommerceApplication.java
@@ -2,10 +2,10 @@ package com.beki7.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class EcommerceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
--- src/main/java/com/beki7/ecommerce/HibernateConfiguration.java
+++ src/main/java/com/beki7/ecommerce/HibernateConfiguration.java
@@ -1,8 +1,15 @@
 package com.beki7.ecommerce;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -52,4 +59,34 @@ public class HibernateConfiguration {
 
     @Value("${entitymanager.packagesToScan}")
     private String PACKAGES_TO_SCAN;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(DRIVER);
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(PACKAGES_TO_SCAN);
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", DIALECT);
+        hibernateProperties.put("hibernate.show_sql", SHOW_SQL);
+        hibernateProperties.put("hibernate.hbm2ddl.auto", HBM2DDL_AUTO);
+        sessionFactory.setHibernateProperties(hibernateProperties);
+
+        return sessionFactory;
+    }
 }
