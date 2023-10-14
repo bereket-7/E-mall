5cf4fd325b3cdb873d3d16a9a0ecff7713f734e2
bereket <bekhonelign@gmail.com>
2023-10-14 12:20:18 -0700

    cart model

--- src/main/java/com/beki7/ecommerce/model/Cart.java
+++ src/main/java/com/beki7/ecommerce/model/Cart.java
@@ -1,5 +1,31 @@
 package com.beki7.ecommerce.model;
 
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name="CART")
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="customer_id")
+    private User customer;
+
+    @ManyToMany
+    @JoinTable(
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    public Cart() {
+        products = new ArrayList<>();
+    }
+
 }
