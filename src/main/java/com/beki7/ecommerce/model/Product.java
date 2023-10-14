fa9f9aa6a73490962bc3389e693b9c683b7ad18d
bereket <bekhonelign@gmail.com>
2023-10-14 12:04:03 -0700

    product model

--- src/main/java/com/beki7/ecommerce/model/Product.java
+++ src/main/java/com/beki7/ecommerce/model/Product.java
@@ -1,4 +1,23 @@
 package com.beki7.ecommerce.model;
 
+import jakarta.persistence.*;
+
+@Entity(name="PRODUCT")
 public class Product {
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    private String name;
+
+    private String image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id",referencedColumnName = "category_id")
+    private Category category;
+    private int quantity;
+    private int price;
+    private int weight;
+    private String description;
 }
