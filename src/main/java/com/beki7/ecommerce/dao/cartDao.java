daf97288864f0493b379db27b895bf9bdfd4d494
bereket <bekhonelign@gmail.com>
2023-10-14 13:12:28 -0700

    cart dao add get methods

--- src/main/java/com/beki7/ecommerce/dao/cartDao.java
+++ src/main/java/com/beki7/ecommerce/dao/cartDao.java
@@ -1,4 +1,29 @@
 package com.beki7.ecommerce.dao;
 
+import com.beki7.ecommerce.model.Cart;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
 public class cartDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf){ this.sessionFactory =sf;}
+
+    @Transactional
+    public Cart addCart(Cart cart) {
+        this.sessionFactory.getCurrentSession().save(cart);
+        return cart;
+    }
+
+    @Transactional
+    public List<Cart> getCarts() {
+        return this.sessionFactory.getCurrentSession().createQuery("from CART").list();
+    }
+
 }
