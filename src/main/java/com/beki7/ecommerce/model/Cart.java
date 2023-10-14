d9dda549868af805f43c9d54e546bc880210278f
bereket <bekhonelign@gmail.com>
2023-10-14 12:23:23 -0700

    cart methods

--- src/main/java/com/beki7/ecommerce/model/Cart.java
+++ src/main/java/com/beki7/ecommerce/model/Cart.java
@@ -28,4 +28,36 @@ public class Cart {
         products = new ArrayList<>();
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
 }
