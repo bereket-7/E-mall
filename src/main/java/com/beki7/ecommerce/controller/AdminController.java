ada7bb0b80141db461524cff7ec0eada5aa931ac
bereket <bekhonelign@gmail.com>
2023-10-18 11:21:22 -0700

    autowired Services

--- src/main/java/com/beki7/ecommerce/controller/AdminController.java
+++ src/main/java/com/beki7/ecommerce/controller/AdminController.java
@@ -1,9 +1,20 @@
 package com.beki7.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.beki7.ecommerce.service.userService;
+import com.beki7.ecommerce.service.categoryService;
+import com.beki7.ecommerce.service.productService;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private userService userService;
+    @Autowired
+    private categoryService categoryService;
+
+    @Autowired
+    private productService productService;
 }
