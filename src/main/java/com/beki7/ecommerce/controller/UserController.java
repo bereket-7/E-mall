c55e96f60806b9c62545ed8cb4a76ddc2ae8bd94
bereket <bekhonelign@gmail.com>
2023-10-18 12:31:18 -0700

    user carts

--- src/main/java/com/beki7/ecommerce/controller/UserController.java
+++ src/main/java/com/beki7/ecommerce/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.beki7.ecommerce.controller;
 
+import com.beki7.ecommerce.service.cartService;
+import com.beki7.ecommerce.model.Cart;
 import com.beki7.ecommerce.model.Product;
 import com.beki7.ecommerce.model.User;
 import jakarta.servlet.http.Cookie;
@@ -22,14 +24,12 @@ public class UserController {
     private productService productService;
 
     @GetMapping("/register")
-    public String registerUser()
-    {
+    public String registerUser() {
         return "register";
     }
 
     @GetMapping("/buy")
-    public String buy()
-    {
+    public String buy() {
         return "buy";
     }
 
@@ -38,16 +38,17 @@ public class UserController {
 
         return "userLogin";
     }
+
     @RequestMapping(value = "userloginvalidate", method = RequestMethod.POST)
     public ModelAndView userlogin(@RequestParam("username") String username, @RequestParam("password") String pass, Model model, HttpServletResponse res) {
 
         System.out.println(pass);
         User u = this.userService.checkLogin(username, pass);
         System.out.println(u.getUsername());
-        if(u.getUsername().equals(username)) {
+        if (u.getUsername().equals(username)) {
 
             res.addCookie(new Cookie("username", u.getUsername()));
-            ModelAndView mView  = new ModelAndView("index");
+            ModelAndView mView = new ModelAndView("index");
             mView.addObject("user", u);
             List<Product> products = this.productService.getProducts();
 
@@ -58,7 +59,7 @@ public class UserController {
             }
             return mView;
 
-        }else {
+        } else {
             ModelAndView mView = new ModelAndView("userLogin");
             mView.addObject("message", "Please enter correct email and password");
             return mView;
@@ -72,22 +73,28 @@ public class UserController {
 
         List<Product> products = this.productService.getProducts();
 
-        if(products.isEmpty()) {
-            mView.addObject("message","No products are available");
-        }else {
-            mView.addObject("products",products);
+        if (products.isEmpty()) {
+            mView.addObject("message", "No products are available");
+        } else {
+            mView.addObject("products", products);
         }
 
         return mView;
     }
 
     @RequestMapping(value = "newuserregister", method = RequestMethod.POST)
-    public String newUseRegister(@ModelAttribute User user)
-    {
+    public String newUseRegister(@ModelAttribute User user) {
         System.out.println(user.getEmail());
         user.setRole("ROLE_NORMAL");
         this.userService.addUser(user);
         return "redirect:/";
+    }
+
+    @GetMapping("carts")
+    public ModelAndView getCartDetail() {
+        ModelAndView mv = new ModelAndView();
+        List<Cart> carts = cartService.getCarts();
+        return carts;
     }
 
 
