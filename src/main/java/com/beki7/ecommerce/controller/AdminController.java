4de778ddd96f9e4d77a476fcdf15f3fda0930317
bereket <bekhonelign@gmail.com>
2023-10-18 11:39:12 -0700

    add products detail

--- src/main/java/com/beki7/ecommerce/controller/AdminController.java
+++ src/main/java/com/beki7/ecommerce/controller/AdminController.java
@@ -153,6 +153,20 @@ public class AdminController {
         mView.addObject("categories",categories);
         return mView;
     }
-
-
+    @RequestMapping(value = "products/add",method=RequestMethod.POST)
+    public String addProduct(@RequestParam("name") String name,@RequestParam("categoryid") int categoryId ,@RequestParam("price") int price,@RequestParam("weight") int weight, @RequestParam("quantity")int quantity,@RequestParam("description") String description,@RequestParam("productImage") String productImage) {
+        System.out.println(categoryId);
+        Category category = this.categoryService.getCategory(categoryId);
+        Product product = new Product();
+        product.setId(categoryId);
+        product.setName(name);
+        product.setCategory(category);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setImage(productImage);
+        product.setWeight(weight);
+        product.setQuantity(quantity);
+        this.productService.addProduct(product);
+        return "redirect:/admin/products";
+    }
 }
