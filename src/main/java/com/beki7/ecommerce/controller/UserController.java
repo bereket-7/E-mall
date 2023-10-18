8f5fe836a18f887c2064a55bfb728dd810689b43
bereket <bekhonelign@gmail.com>
2023-10-18 12:04:19 -0700

    user controller

--- src/main/java/com/beki7/ecommerce/controller/UserController.java
+++ src/main/java/com/beki7/ecommerce/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.beki7.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.beki7.ecommerce.service.userService;
+import com.beki7.ecommerce.service.productService;
+
+@Controller
 public class UserController {
+    @Autowired
+    private userService userService;
+    @Autowired
+    private productService productService;
 }
