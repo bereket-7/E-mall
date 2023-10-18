08b9439eb7e3d5cf4eaa16cc258b32b6a2658bbc
bereket <bekhonelign@gmail.com>
2023-10-18 12:05:16 -0700

    buy,register and login end points

--- src/main/java/com/beki7/ecommerce/controller/UserController.java
+++ src/main/java/com/beki7/ecommerce/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import com.beki7.ecommerce.service.userService;
 import com.beki7.ecommerce.service.productService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
@@ -11,4 +13,26 @@ public class UserController {
     private userService userService;
     @Autowired
     private productService productService;
+
+    @GetMapping("/register")
+    public String registerUser()
+    {
+        return "register";
+    }
+
+    @GetMapping("/buy")
+    public String buy()
+    {
+        return "buy";
+    }
+
+
+    @GetMapping("/")
+    public String userlogin(Model model) {
+
+        return "userLogin";
+    }
+
+
+
 }
