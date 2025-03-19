9839a05d652a597050b27f3b6428db3684fdf7d9
bereket-7 <bekhonelign@gmail.com>
2025-03-19 22:57:02 +0300

    project structure refactor

---
+++ order-service/src/test/java/com/beki7/ecommerce/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beki7.ecommerce.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
