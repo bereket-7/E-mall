8634a4a8cd5c9aefeb4a2d72f981ff29bec08a4c
bereket-7 <bekhonelign@gmail.com>
2025-03-19 23:55:13 +0300

    payment , inventory microservice

---
+++ inventory-service/src/test/java/com/beki7/ecommerce/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beki7.ecommerce.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-service/src/test/java/com/beki7/ecommerce/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beki7.ecommerce.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
